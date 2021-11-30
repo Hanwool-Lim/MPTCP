ef23aece2933c6d951a74cf54f2ac40495a29367
Hanwool-Lim <60589231+Hanwool-Lim@users.noreply.github.com>
2021-11-30 22:14:55 +0900

    Update lia.c

--- lia.c
+++ lia.c
@@ -312,11 +312,13 @@ static void update_params(struct sock *sk)
 	struct illinois *ca = inet_csk_ca(sk);
 
 	if (tp->snd_cwnd < win_thresh) {
+		ca->alpha = ALPHA_BASE;
 		ca->beta = BETA_BASE;
 	} else if (ca->cnt_rtt > 0) {
 		u32 dm = max_delay(ca);
 		u32 da = avg_delay(ca);
-
+		
+		ca->alpha = alpha(ca, da, dm);
 		ca->beta = beta(da, dm);
 	}
 
