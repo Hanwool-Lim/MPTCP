104aa74c7f64c38948764c6f62d1b1f2163fc7fa
Hanwool-Lim <60589231+Hanwool-Lim@users.noreply.github.com>
2021-11-30 20:22:16 +0900

    Update lia.c

--- lia.c
+++ lia.c
@@ -334,6 +334,8 @@ static void mptcp_ccc_set_state(struct sock *sk, u8 ca_state)
 	if (!mptcp(tcp_sk(sk)))
 		return;
 	
+	mptcp_set_forced(mptcp_meta_sk(sk), 1);
+	
 	if (ca_state == TCP_CA_Loss) {
 		ca->beta = BETA_BASE;
 		ca->rtt_low = 0;
