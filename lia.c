c30838dce4bca7a4a80ed2ceaec0b4ffa346fd6c
Hanwool-Lim <60589231+Hanwool-Lim@users.noreply.github.com>
2021-12-02 14:55:30 +0900

    Update lia.c

--- lia.c
+++ lia.c
@@ -192,7 +192,7 @@ static u32 tcp_illinois_ssthresh(struct sock *sk)
 	struct illinois *ca = inet_csk_ca(sk);
 
 	/* Multiplicative decrease */
-	return max(tp->snd_cwnd - ((tp->snd_cwnd * ca->beta) >> BETA_SHIFT), (tp->snd_cwnd*7)/8);
+	return max(tp->snd_cwnd - ((tp->snd_cwnd * ca->beta) >> BETA_SHIFT), (tp->snd_cwnd*9)/10);
 }
 
 
