db17cfa6ed0ef6fded30630ba415df468469241b
Hanwool-Lim <60589231+Hanwool-Lim@users.noreply.github.com>
2021-12-05 12:23:19 +0900

    Update lia.c

--- lia.c
+++ lia.c
@@ -319,10 +319,12 @@ static u32 beta(u32 da, u32 dm)
 	d2 = dm / 20;
 	d3 = (8 * dm) / 10;
 	
-	if (da >= d3 || d3 <= d2)
-		return BETA_MAX;
 	if (da <= d2)
 		return BETA_MIN;
+	
+	if (da >= d3 || d3 <= d2)
+		return BETA_MAX;
+
 
 	return (BETA_MIN * d3 - BETA_MAX * d2 + (BETA_MAX - BETA_MIN) * da) / (d3 - d2);
 }
